600e2bf8de5025c13a79695598b84424eae0e927
abiton198 <abitonp@gmail.com>
2022-11-11 03:30:00 +0200

    .

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -30,25 +30,25 @@ export default function Skills() {
 
             <div className='skill'>
                 <img src={react} alt='' className='img-skill'/>
-                <p>Javascript Library that makes code clean, readable, reusable and effecient 
+                <p className='para-skill'>Javascript Library that makes code clean, readable, reusable and effecient 
                     for production. Increases good code practises. </p>
             </div>
 
             <div className='skill'>
                 <img src={git} alt='' className='img-skill'/>
-                <p>A code hosting platform for version control and collaboration.</p>
+                <p className='para-skill'>A code hosting platform for version control and collaboration.</p>
             </div>
 
             <div className='skill'>
                 <img src={git2} alt='' className='img-skill'/>
-                <p>Git is a free and open source distributed version control system designed 
+                <p className='para-skill'>Git is a free and open source distributed version control system designed 
                     to handle everything from small to very large projects with speed and efficiency.
                 </p>
             </div>
 
             <div className='skill'>
                 <img src={netlify} alt='' className='img-skill'/>
-                <p>A web domain hosting platform, enbabling one to customize and own domains.</p>
+                <p className='para-skill'>A web domain hosting platform, enbabling one to customize and own domains.</p>
             </div>
 
 
